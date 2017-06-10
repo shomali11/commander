0ed7a8253b799589c566c26166be9ff8aae1be8d
Raed Shomali <shomali11@gmail.com>
2017-06-09 21:40:33 -0400

    Added commander

---
+++ commander_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMatch(t *testing.T) {
+	properties, isMatch := NewCommand("").Match("ping")
+	assert.False(t, isMatch)
+	assert.Nil(t, properties)
+
+	properties, isMatch = NewCommand("").Match("")
+	assert.False(t, isMatch)
+	assert.Nil(t, properties)
+
+	properties, isMatch = NewCommand("ping").Match("ping")
+	assert.True(t, isMatch)
+	assert.NotNil(t, properties)
+
+	properties, isMatch = NewCommand("ping").Match("pong")
+	assert.False(t, isMatch)
+	assert.Nil(t, properties)
+
+	properties, isMatch = NewCommand("echo <word>").Match("echo")
+	assert.True(t, isMatch)
+	assert.NotNil(t, properties)
+
+	properties, isMatch = NewCommand("echo <word>").Match("echo hey")
+	assert.True(t, isMatch)
+	assert.Equal(t, properties.StringParam("word", ""), "hey")
+
+	properties, isMatch = NewCommand("repeat <word> <number>").Match("repeat hey 5")
+	assert.True(t, isMatch)
+	assert.Equal(t, properties.StringParam("word", ""), "hey")
+	assert.Equal(t, properties.IntegerParam("number", 0), 5)
+
+	properties, isMatch = NewCommand("repeat <word> <number>").Match("repeat hey")
+	assert.True(t, isMatch)
+	assert.Equal(t, properties.StringParam("word", ""), "hey")
+	assert.Equal(t, properties.IntegerParam("number", 0), 0)
+}
+
+func TestIsParameter(t *testing.T) {
+	assert.True(t, IsParameter("<value>"))
+	assert.True(t, IsParameter("<123>"))
+	assert.True(t, IsParameter("<value123>"))
+	assert.False(t, IsParameter("value>"))
+	assert.False(t, IsParameter("<value"))
+	assert.False(t, IsParameter("value"))
+	assert.False(t, IsParameter(""))
+}
