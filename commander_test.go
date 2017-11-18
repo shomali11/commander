4e3c77c8fc1962df2da29296972d1651d56d04a8
Raed Shomali <shomali11@gmail.com>
2017-11-17 22:12:42 -0500

    Improvements

--- commander_test.go
+++ commander_test.go
@@ -5,6 +5,15 @@ import (
 	"testing"
 )
 
+func TestTokenize(t *testing.T) {
+	tokens := NewCommand("search <pattern>").Tokenize()
+	assert.Equal(t, len(tokens), 2)
+	assert.Equal(t, tokens[0].Word, "search")
+	assert.False(t, tokens[0].IsParameter)
+	assert.Equal(t, tokens[1].Word, "pattern")
+	assert.True(t, tokens[1].IsParameter)
+}
+
 func TestMatch(t *testing.T) {
 	properties, isMatch := NewCommand("").Match("ping")
 	assert.False(t, isMatch)
@@ -26,6 +35,10 @@ func TestMatch(t *testing.T) {
 	assert.False(t, isMatch)
 	assert.Nil(t, properties)
 
+	properties, isMatch = NewCommand("search all").Match("search for all")
+	assert.False(t, isMatch)
+	assert.Nil(t, properties)
+
 	properties, isMatch = NewCommand("help").Match("Could you help me?")
 	assert.True(t, isMatch)
 	assert.NotNil(t, properties)
@@ -37,6 +50,10 @@ func TestMatch(t *testing.T) {
 	properties, isMatch = NewCommand("echo <word>").Match("echo hey")
 	assert.True(t, isMatch)
 	assert.Equal(t, properties.StringParam("word", ""), "hey")
+
+	properties, isMatch = NewCommand("search <pattern>").Match("search *")
+	assert.True(t, isMatch)
+	assert.Equal(t, properties.StringParam("pattern", ""), "*")
 
 	properties, isMatch = NewCommand("repeat <word> <number>").Match("repeat hey 5")
 	assert.True(t, isMatch)
